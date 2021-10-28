96bc560b6d97a9f83de9271d898a345e862547ca
GameTec-live <66077766+GameTec-live@users.noreply.github.com>
2021-10-28 18:41:21 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -14,6 +14,9 @@ from dotenv import load_dotenv
 from discord.ext import commands
 
 
+
+#invite link https://discord.com/api/oauth2/authorize?client_id=894582921946599474&permissions=8&redirect_uri=http%3A%2F%2Flocalhost%2Fcallback%2F&scope=bot%20applications.commands
+
 #load envs
 load_dotenv()
 db_user = os.getenv("DB_USER")
@@ -54,6 +57,79 @@ async def on_ready():
     print('------')
 
 
+#application
+async def apply(ctx, user: discord.User):
+    try:
+        app = json.load(open(f"./applications.json"))
+    except:
+        app = {}
+    if app != "" and app != None:
+        embed=discord.Embed(title="Application", color=0x68b38c)
+        embed.add_field(name="What do you want to apply for?", value="please select", inline=False)
+        options = []
+        for application in app:#load json files
+            options.append(discord.ui.SelectOption(label=app[application]["name"], value=application),)
+        components = discord.ui.MessageComponents(
+            discord.ui.ActionRow(
+                discord.ui.SelectMenu(
+                    custom_id="select",
+                    options=options,
+                ),
+            ),
+            discord.ui.ActionRow(
+                discord.ui.Button(label="abort", custom_id="abort"),
+            ),
+        )
+
+
+        await user.send(embed=embed,components=components)
+
+        def check(interaction: discord.Interaction):
+            return True
+        interaction = await client.wait_for("component_interaction", check=check)
+        components.disable_components()
+        await interaction.response.edit_message(components=components)
+        if interaction.component.custom_id == "abort":
+            embed=discord.Embed(title="Application", color=0x68b38c)
+            embed.add_field(name="Aborted", value="Application aborted", inline=False)
+            await user.send(embed=embed)
+            return
+        print(interaction.values[0])
+        for application in app:#load json files
+            if interaction.values[0] == application:
+                embed=discord.Embed(title="Application", color=0x68b38c)
+                embed.add_field(name="Applying for " + app[application]["name"], value=app[application]["description"], inline=False)
+                components = discord.ui.MessageComponents(discord.ui.ActionRow(discord.ui.Button(label="abort", custom_id="abort"),discord.ui.Button(label="continue", custom_id="continue"),),)
+                await user.send(embed=embed, components=components)
+                interaction = await client.wait_for("component_interaction", check=check)
+                components.disable_components()
+                await interaction.response.edit_message(components=components)
+                if interaction.component.custom_id == "abort":
+                    embed=discord.Embed(title="Application", color=0x68b38c)
+                    embed.add_field(name="Aborted", value="Application aborted", inline=False)
+                    await user.send(embed=embed)
+                    return
+                if interaction.component.custom_id == "continue":
+                    embed=discord.Embed(title="Application", color=0x68b38c)
+                    embed.add_field(name="Applying for " + app[application]["name"], value=app[application]["description"], inline=False)
+                    embed.add_field(name="Please enter your application", value="app", inline=False)
+                    await user.send(embed=embed)
+                    final=discord.Embed(title="Application - " + str(interaction.user), color=0x68b38c)
+                    for field in app[application]["questions"]:
+                        embed=discord.Embed(title="Application", color=0x68b38c)
+                        embed.add_field(name=field["question"], value="awnser below", inline=False)
+                        await user.send(embed=embed)
+                        awnser = await client.wait_for("message", timeout = 30, check=lambda message: message.author == user)
+                        final.add_field(name=field["question"], value=awnser.content, inline=False)
+                    channel = client.get_channel(int(app[application]["channel"]))
+                    await channel.send(embed=final)
+                    return
+                return
+    else:
+        await user.send("There are no applications available")
+
+
+
 #handle slash commands
 @client.event
 async def on_slash_command(interaction: discord.Interaction):
@@ -70,7 +146,12 @@ async def on_slash_command(interaction: discord.Interaction):
         await interaction.response.send_message(embed=embed)
 
     if command_name == "apply":
-        await interaction.response.send_message("coming soon")
+        embed=discord.Embed(title="Application", color=0x68b38c)
+        embed.add_field(name="Application started", value=interaction.user, inline=False)
+        await interaction.response.send_message(embed=embed)
+        await apply(interaction.response, interaction.user)
+        print("applied")
+
 
 
     #custom commands
@@ -85,7 +166,7 @@ async def on_slash_command(interaction: discord.Interaction):
                 except:
                     pass
             if command["embed"] != "":
-                embed = discord.Embed(title=command["embed"]["title"], description=command["embed"]["description"], color=0x00ff00)
+                embed = discord.Embed(title=command["embed"]["title"], description=command["embed"]["description"], color=0x68b38c)
                 for field in command["embed"]["fields"]:
                     embed.add_field(name=field["name"], value=field["value"], inline=False)
                 embed.set_footer(text=command["embed"]["footer"])
