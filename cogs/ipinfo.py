b415c1f9905ede208bf037fcd0358b384f808052
GameTec_live <benedikt.werner08@gmail.com>
2022-07-07 18:35:54 +0200

    added an IP cog

---
+++ cogs/ipinfo.py
@@ -0,0 +1,22 @@
+from discord.ext import commands
+import discord
+from requests import get
+
+
+class IPinfo(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.last_ip = None
+
+    @commands.command(application_command_meta=commands.ApplicationCommandMeta())
+    async def ipinfo(self, ctx):
+        ip = get('https://api.ipify.org').text
+        embed = discord.Embed(title="IP Info", description="Current IP:" + ip, color=0x00FF00)
+        if self.last_ip != ip:
+            embed.add_field(name="IP Changed", value="From " + self.last_ip + " to " + ip, inline=False)
+            self.last_ip = ip
+        await ctx.interaction.response.send_message(embed=embed)
+
+
+def setup(client):
+    client.add_cog(IPinfo(client))
