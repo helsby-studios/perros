2a9ce6e90e7ab039f14877652a77c43ab04db3e8
GameTec_live <benedikt.werner08@gmail.com>
2022-07-08 08:18:27 +0200

    hopefully fixed ipcog

--- cogs/ipinfo.py
+++ cogs/ipinfo.py
@@ -6,21 +6,13 @@ from requests import get
 class IPinfo(commands.Cog):
     def __init__(self, client):
         self.client = client
-        self.last_ip = get("https://api.ipify.org").text
 
     @commands.command(application_command_meta=commands.ApplicationCommandMeta())
     async def ipinfo(self, ctx):
         ip = get("https://api.ipify.org").text
         embed = discord.Embed(
-            title="IP Info", description="Current IP:" + ip, color=0x00FF00
+            title="IP Info", description="Current IP:" + str(ip), color=0x00FF00
         )
-        if self.last_ip != ip:
-            embed.add_field(
-                name="IP Changed",
-                value="From " + self.last_ip + " to " + ip,
-                inline=False,
-            )
-            self.last_ip = ip
         await ctx.interaction.response.send_message(embed=embed)
 
 
