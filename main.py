d093e7bb072b4526cb6a1004510eb6e2da9c1579
GameTec_live <benedikt.werner08@gmail.com>
2022-09-02 10:20:14 +0200

    pleased linter

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 from quart import Quart, redirect, render_template, url_for
 from quart_discord import (
     DiscordOAuth2Session,
-    requires_authorization,
     Unauthorized,
+    requires_authorization,
 )
 
 # start bot
@@ -22,7 +22,7 @@ dc_callback_uri = os.getenv("DISCORD_CALLBACK_URI")
 
 # initiailize app
 app = Quart(__name__)
-app.secret_key = b"TiV7TDXK39uNgaLtwB7EXkfoZfQpJ615vDKqgPHaMXoFsJUBWMebg4pAQhj9cyWc8AAKVXQ3ha1jh12DsCoW58KQL3T4KcaPHQmM"  # key used to sign session cookies
+app.secret_key = b"TiV7TDXK39uNgaLtwB7EXkfoZfQpJ615vDKqg"  # key used to sign session cookies
 app.config["DISCORD_CLIENT_ID"] = dc_client_id
 app.config["DISCORD_CLIENT_SECRET"] = dc_client_secret
 app.config["DISCORD_REDIRECT_URI"] = dc_callback_uri
@@ -90,7 +90,8 @@ async def dashboard_server(guild_id):
     guild = await bot.get_guild(guild_id)
     if guild is None:
         return redirect(
-            f'https://discord.com/oauth2/authorize?&client_id={app.config["DISCORD_CLIENT_ID"]}&scope=bot&permissions=534723951680&guild_id={guild_id}&response_type=code&redirect_uri={app.config["DISCORD_REDIRECT_URI"]}'
+            f'https://discord.com/oauth2/authorize?&client_id={app.config["DISCORD_CLIENT_ID"]}&scope=bot&permissions'
+            f'=534723951680&guild_id={guild_id}&response_type=code&redirect_uri={app.config["DISCORD_REDIRECT_URI"]} '
         )
     # return guild["name"]
     return await render_template("ui.html", guild=guild["name"], guild_id=guild_id)
